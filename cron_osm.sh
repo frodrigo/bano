c543244242182ab9db09c76432b171b3394c5dab
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-02-19 15:50:00 +0100

    Does not log direcly from script

--- cron_osm.sh
+++ cron_osm.sh
@@ -9,19 +9,18 @@ pip install -e .
 source config
 
 lockfile=imposm.lock
-LOGFILE=${SCRIPT_DIR}/cron.log
 
-echo `date`>> ${LOGFILE}
-echo debut >> ${LOGFILE}
+echo `date`
+echo debut
 
 if test -f ${lockfile}
 then
   diff_age=$((`date +%s` - `stat -c %Y $lockfile`))
   if [ $diff_age -gt 7200 ];then
-    echo "Effacement du lock" >> ${LOGFILE}
+    echo "Effacement du lock"
     rm ${lockfile}
   else
-    echo `date`" : Process deja en cours" >> ${LOGFILE}
+    echo `date`" : Process deja en cours"
     exit 1
   fi
 fi
@@ -29,21 +28,19 @@ fi
 
 touch ${lockfile}
 
-echo `date`" : Osmosis" >> ${LOGFILE}
+echo `date`" : Osmosis"
 osmosis --rri workingDirectory=${DOWNLOAD_DIR} --simc --wxc ${DOWNLOAD_DIR}/changes.osc.gz
-echo `date`" : Imposm" >> ${LOGFILE}
+echo `date`" : Imposm"
 imposm diff \
   -config imposm.config \
   -cachedir $IMPOSM_CACHE_DIR \
   -connection postgis://$PGCON_BANO?prefix=NONE \
   -dbschema-production osm \
   ${DOWNLOAD_DIR}/changes.osc.gz
-echo `date`" : Osm2pgsql" >> ${LOGFILE}
+echo `date`" : Osm2pgsql"
 osm2pgsql -a -S osm2pgsql.style -s -l -d bano -U cadastre -p osm2pgsql $DOWNLOAD_DIR/changes.osc.gz
 
 rm ${lockfile}
 
-echo `date` >> ${LOGFILE}
-echo fin >> ${LOGFILE}
-
-tail -1000 ${LOGFILE} > foo.bar.foo && mv foo.bar.foo ${LOGFILE}
+echo `date`
+echo fin

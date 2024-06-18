ba7c085fc0698fd14a35aeca4ad73f396bbd0c51
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-06-18 13:23:40 +0200

    Import into public schema

--- bano/sql/create_base.sql
+++ bano/sql/create_base.sql
@@ -2,4 +2,4 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS hstore;
 
 -- tables Imposm dans le schema osm
-ALTER ROLE cadastre IN DATABASE bano SET search_path TO public,osm;
+ALTER ROLE postgres SET search_path TO public,osm;

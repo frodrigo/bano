8c5c611c57c51d30fe2221d02a71de3bfe89fc32
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-07-07 16:50:43 +0200

    LOCAL CHANGES

--- cron_bano.sh
+++ cron_bano.sh
@@ -11,8 +11,8 @@ source config
 bano="python -m bano"
 
 # Sources
-cat deplist.txt | parallel -j $PARALLEL_JOBS export LANG=$LANG\; $bano charge_ban {1}
-cat deplist.txt | parallel -j $PARALLEL_JOBS export LANG=$LANG\; $bano charge_ld_cadastre {1}
+cat deplist.txt | parallel -j $PARALLEL_JOBS export LANG=$LANG\; $bano charge_ban --forceload {1}
+cat deplist.txt | parallel -j $PARALLEL_JOBS export LANG=$LANG\; $bano charge_ld_cadastre --forceload {1}
 $bano menage_noms_ban
 $bano update_bis_table
 

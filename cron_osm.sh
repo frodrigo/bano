7826d96e9947f04d76eac758b4bef4a2bcebd1ce
Vincent de Château-Thierry <vdct@laposte.net>
2024-11-03 22:00:55 +0000

    Nouveau chargement osm2pgsql : voies et limites

--- cron_osm.sh
+++ cron_osm.sh
@@ -12,18 +12,19 @@ pip install -e .
 source $SCRIPT_DIR/config
 
 lockfile=${SCRIPT_DIR}/imposm.lock
+LOGFILE=${SCRIPT_DIR}/cron.log
 
-echo `date`>> $SCRIPT_DIR/cron.log
-echo debut >> $SCRIPT_DIR/cron.log
+echo `date`>> ${LOGFILE}
+echo debut >> ${LOGFILE}
 
 if test -f ${lockfile}
 then
   diff_age=$((`date +%s` - `stat -c %Y $lockfile`))
   if [ $diff_age -gt 7200 ];then
-    echo "Effacement du lock" >> $SCRIPT_DIR/cron.log
+    echo "Effacement du lock" >> ${LOGFILE}
     rm ${lockfile}
   else
-    echo `date`" : Process deja en cours" >> $SCRIPT_DIR/cron.log
+    echo `date`" : Process deja en cours" >> ${LOGFILE}
     exit 0
   fi
 fi
@@ -31,10 +32,16 @@ fi
 
 touch ${lockfile}
 
-osmosis --rri workingDirectory=${DOWNLOAD_DIR} --wxc ${DOWNLOAD_DIR}/changes.osc.gz
+echo `date`" : Osmosis" >> ${LOGFILE}
+osmosis --rri workingDirectory=${DOWNLOAD_DIR} --simc --wxc ${DOWNLOAD_DIR}/changes.osc.gz
+echo `date`" : Imposm" >> ${LOGFILE}
 imposm diff -config $SCRIPT_DIR/imposm.config -cachedir $IMPOSM_CACHE_DIR -dbschema-production osm ${DOWNLOAD_DIR}/changes.osc.gz
+echo `date`" : Osm2pgsql" >> ${LOGFILE}
+osm2pgsql -a -S osm2pgsql.style -s -l -d bano -U cadastre -p osm2pgsql $DOWNLOAD_DIR/changes.osc.gz
 
 rm ${lockfile}
 
-echo `date` >> $SCRIPT_DIR/cron.log
-echo fin >> $SCRIPT_DIR/cron.log
+echo `date` >> ${LOGFILE}
+echo fin >> ${LOGFILE}
+
+tail -1000 ${LOGFILE} > foo.bar.foo && mv foo.bar.foo ${LOGFILE}
---
+++ osm2pgsql_load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/config
+
+PBF_URL=${1:-http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf}
+PBF_FILE=$(basename "$PBF_URL")
+
+mkdir -p $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+wget -NS $PBF_URL
+
+cd $SCRIPT_DIR
+osm2pgsql -S osm2pgsql.style -s -l -d bano -U cadastre -p osm2pgsql $DOWNLOAD_DIR/$PBF_FILE

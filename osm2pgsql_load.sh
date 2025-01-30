144979df6b7e1a29de89b4c50cf0ba1acfc66731
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-01-30 15:55:24 +0100

    Use config for osm2pgsql

--- osm2pgsql_load.sh
+++ osm2pgsql_load.sh
@@ -11,6 +11,6 @@ mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
 wget -NS $PBF_URL
 
-osm2pgsql -S $BANO_DIR/osm2pgsql.style -l -d bano -U cadastre -p osm2pgsql $DOWNLOAD_DIR/$PBF_FILE
+osm2pgsql -S $BANO_DIR/osm2pgsql.style -l -d "postgresql://${PGCON_BANO}" -p osm2pgsql $DOWNLOAD_DIR/$PBF_FILE
 
 $pgsql_BANO -f $BANO_DIR/sql/finalisation_osm2pgsql.sql

afa36bdf3cb7cd08542a785ecf24fc35f9a36756
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-02-12 17:32:36 +0100

    Hack pour contourner des problemes de la BAN sur Bordeaux

--- bano/sql/menage_noms_ban.sql
+++ bano/sql/menage_noms_ban.sql
@@ -10,3 +10,21 @@ WHERE nom_voie LIKE '% '                       OR
       nom_voie LIKE '%'||CHR(34)||'%'          OR
       nom_voie LIKE CHR(39)||'%'               OR
       nom_voie LIKE '%'||CHR(39);
+
+
+-- Supprime de la BAN des voies de mêmes noms sur la même commune fusionnée par erreur
+-- https://github.com/osm-fr/bano/issues/439
+DELETE FROM
+   ban
+WHERE
+   code_insee='33063' AND
+   nom_voie IN (
+      'Rue Camille Saint-Saëns',
+      'Rue Corot',
+      'Rue Jean-Jacques Rousseau',
+      'Rue Kléber',
+      'Rue de Lacanau',
+      'Rue Maréchal Joffre',
+      'Rue Pierre Loti'
+   )
+;
